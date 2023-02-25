f2a8b71555d93b807ef2a13edcd9fc1ec8ee4158
Elias Hezron <eliashezron@Eliass-MacBook-Pro.local>
2023-02-25 16:31:01 +0300

    backend contracts added

---
+++ backend/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+}
+require("dotenv").config()
+
+const PRIVATE_KEY =
+  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "hyperspace",
+  networks: {
+    hyperspace: {
+      chainId: 3141,
+      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+}
